faeb2b1574ba4f8b20f624afa54c4bb3ad50c8ab
Yusuf Hafidz <yhafidz@gmail.com>
2022-04-13 00:43:18 +0700

    Renew Open/Save method
    Now with window title renaming

--- GUI.py
+++ GUI.py
@@ -9,9 +9,10 @@ root = tk.Tk()
 # ===== #
 
 # Head
-root.title("Text Editor")
+CONST_app_name = "Text Editor"
+root.title(CONST_app_name)
 
-# Window Size
+# Root Window Size
 root.geometry("800x800")
 
 # Root Grid Configuration
@@ -19,7 +20,7 @@ root.rowconfigure(0, weight=1,minsize=100)
 root.columnconfigure(0, weight=0, minsize=100)
 root.columnconfigure(1, weight=1, minsize=100)
 
-# Frames
+# Root Frames
 frm_side_menu = tk.Frame(root,
     background="#4f4f4f"
     )
@@ -55,50 +56,58 @@ btn_save.grid(row=1, column=0, padx=10,pady=5)
 txt_main_editor = tk.Text(frm_editor,
     background="#3f3f3f",
     foreground="white",
-    borderwidth=0
+    borderwidth=0,
+    insertbackground="white"
 )
-txt_main_editor.pack(fill=tk.BOTH, expand=True)
+txt_main_editor.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
 # ===== #
 # Logics/Commands
 # ===== #
 
 # Global Var
-f = None                # Current wokring file
-file_opened = False     # File opened status
+f_opened = False    # File open status
+f_name = None       # Opened file name
 
 # Callback Function
 def open_file():
-    global f
-    global file_opened
-    if file_opened:
-        f.close()
+    global f_opened
+    global f_name
+
     fd = filedialog.askopenfilename(
-        filetypes=[("Text Files", "*.txt")],
+        filetypes=[("Text Files", "*.txt"), ("All Files","*.*")],
         title="Open Text File..."
     )
     if fd:
-        f = open(fd, "r")
-        txt_main_editor.delete("1.0", tk.END)
-        txt_main_editor.insert("1.0", f.read())
-        file_opened = True
+        # If a file is selected
+        with open(fd, "r") as f:
+            txt_main_editor.delete("1.0", tk.END)
+            txt_main_editor.insert("1.0", f.read())
+            root.title(f"{CONST_app_name} - {f.name}")
+            f_name = f.name
+            f_opened = True
 
-def save_file():
-    global f
-    global file_opened
-    if file_opened:
-        fw = open(f.name, "w")
-        fw.write(txt_main_editor.get("1.0", tk.END)[:-1])
-        fw.close()
+def save_file() -> int: 
+    global f_opened
+    global f_name
+
+    if f_opened:
+        # Save opened file
+        with open(f_name, "w") as fw:
+            fw.write(txt_main_editor.get("1.0", tk.END)[:-1])
     else:
+        # If no file is opened, do save as..
         fd = filedialog.asksaveasfile(
             filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
             title="Save as...",
             defaultextension="*.*"
         )
-        f = open(fd.name, "w")
-        f.write(txt_main_editor.get("1.0", tk.END)[:-1])
-        file_opened = True
+        if fd:
+            with open(fd.name, "w") as fw:
+                fw.write(txt_main_editor.get("1.0", tk.END)[:-1]) # [:-1] avoid 'newline' from tk.END
+                root.title(f"{CONST_app_name} - {fw.name}")
+                f_opened = True
+                f_name = fw.name
 
 # Buttons Command
 btn_open["command"] = open_file
